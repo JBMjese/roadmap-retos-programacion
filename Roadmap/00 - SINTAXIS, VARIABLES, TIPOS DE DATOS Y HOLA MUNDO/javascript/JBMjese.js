b3751592237b0e3ff0d3757d57acb8aa6edf01c8
JBMjese <6935@holbertonstudents.com>
2024-05-16 20:40:51 -0500

    #00-JavaScrip

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/JBMjese.js
@@ -0,0 +1,25 @@
+// Los comentarios en JavaScrip se escriben con '//' para una sola linea.
+
+/* Y de esta forma para un comentario 
+de multiples lineas. */
+
+// https://www.javascript.com/ aqui encuentras inf basica sobre el lenguaje, tutoriales y recursos.
+
+/* Este es el sitio web de Mozilla Developer Network (MDN)
+https://developer.mozilla.org/es/docs/Learn/Getting_started_with_the_web/JavaScript_basics
+aqui encontraras una sobre fundamentos de JavaScrip. */
+
+let Minombre = "Jesenia"; // Declaracion de variable con let.
+const EDAD = 33; // Declaracion de una costante con cons.
+
+// Tipos de datos primitivos en JavaScrip
+
+let nombre = "Pepito"; // String
+let edad = 20; // Number
+let sinValor; // Undefined
+let esVerdadero = true; //Boleano
+let valorNulo = null; //Null
+const ValorGrande = 9007199254740991n; // Bigint
+const simboloUnico = Symbol('descripcion'); // Symbol
+
+console.log("Hola, [JavaScrip]!");
